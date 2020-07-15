26811fe88e36bc0ad72b49ba52e57180dda10034
SongWei <leo_songwei@outlook.com>
2020-07-15 22:10:02 +1000

    save

--- avcodec.lisp
+++ avcodec.lisp
@@ -1,21 +1,4 @@
-(ql:quickload 'cffi)
-(ql:quickload 'cffi-grovel)
-
-(use-package :cffi)
-(load (cffi-grovel:process-grovel-file "ffmpeg-grovel.lisp" "/dev/shm/_ffmpeg.o"))
-
-(define-condition c-ffmpeg-condition (error)
-  ((message :initarg :message)
-   (data :initarg :data))
-  (:report (lambda (condition stream)
-             (format stream "message: ~A~%data: ~A~%"
-                     (slot-value condition 'message)
-                     (slot-value condition 'data)))))
-(defun c-ffmpeg-condition (message &optional data)
-  (error (make-condition 'c-ffmpeg-condition
-                         :message message
-                         :data data)))
-;; (c-ffmpeg-condition "no!!!!" "I'am your father!")
+(load (cffi-grovel:process-grovel-file "avcodec-grovel.lisp" "/dev/shm/_ffmpeg.o"))
 
 ;; load ffmpeg libraries
 (load-foreign-library "libavcodec.so")
@@ -75,35 +58,3 @@
     (if (null-pointer-p ptr)
         (c-ffmpeg-condition "Could not allocat AVPacket"))
     (make-ffmpeg-av-packet :ptr ptr)))
-
-;; ----------------------------------------------
-
-(defun assign-to-foreign (ptr root-type value-tree)
-  (assert (= 0 (mod (length value-tree) 2)))
-  (let ((values (copy-tree value-tree)))
-    (dotimes (_ (/ (length values) 2))
-      (let* ((slot-name (pop values))
-             (slot-value (pop values))
-             (slot-type (foreign-slot-type root-type slot-name)))
-        (if (consp slot-value)
-            (let ((obj (null-pointer)))
-              (unwind-protect
-                   (progn (setf obj (convert-to-foreign slot-value slot-type))
-                          (setf (foreign-slot-value ptr root-type slot-name) obj))
-                (foreign-free obj)))
-            (setf (foreign-slot-value ptr root-type slot-name) slot-value))))))
-
-(let* ((c (ffmpeg-avcodec-alloc-context3
-           (ffmpeg-find-encoder-by-name "libx264")))
-       (ptr (ffmpeg-avcodec-context-ptr c))
-       (type '(:struct c-ffmpeg-avcodeccontext)))
-  (assign-to-foreign ptr type
-                     '(width 1920
-                       height 1080
-                       bit-rate 1500000
-                       bit-rate-tolerance 500000
-                       time-base (num 1 den 20)
-                       framerate (num 20 den 1)
-                       av-pixel-format :AV_PIX_FMT_RGB24))
-  (convert-from-foreign ptr type))
-
--- avdevice.lisp
+++ avdevice.lisp
@@ -1,12 +1,40 @@
-(load-foreign-library "libavdevice.so")
 (load (cffi-grovel:process-grovel-file "avdevice-grovel.lisp" "/dev/shm/_avdevice.o"))
-
+(load-foreign-library "libavdevice.so")
 
 (defcfun ("avdevice_configuration" c-ffmpeg-avdevice_configuration) :string)
 ;; (c-ffmpeg-avdevice_configuration)
 
 (defcfun ("avformat_alloc_context" c-ffmpeg-avformat_alloc_context) (:pointer (:struct c-ffmpeg-AVFormatContext)))
+;; (c-ffmpeg-avformat_alloc_context)
 
 (defcfun ("avdevice_list_devices" c-ffmpeg-avdevice_list_devices) :int
   (context (:pointer (:struct c-ffmpeg-AVFormatContext)))
   (device_list (:pointer (:pointer (:struct c-ffmpeg-AVDeviceInfoList)))))
+
+;; --------------------------------------------
+
+(defstruct ffmpeg-avformat-context
+  (ptr #.(null-pointer) :type #.(type-of (null-pointer))))
+
+(defun ffmpeg-avformat-alloc-context ()
+  (let ((ptr (c-ffmpeg-avformat_alloc_context)))
+    (if (null-pointer-p ptr)
+        (c-ffmpeg-condition "Could not allocate AVFormatContext"))
+    (make-ffmpeg-avformat-context :ptr ptr)))
+;; (ffmpeg-avformat-alloc-context)
+
+(defstruct ffmpeg-avdevice-info-list
+  (ptr #.(null-pointer) :type #.(type-of (null-pointer))))
+
+;; ptr = []
+;; ptr_actual = ref(ptr)
+
+(defun ffmpeg-avdevice-list-devices (ffmpeg-avformat-context)
+  (let ((context-ptr (ffmpeg-avformat-context-ptr ffmpeg-avformat-context)))
+    (with-foreign-object (outer-ptr :pointer)
+      (let ((inner-ptr (foreign-alloc :pointer)))
+        (setf (mem-ref outer-ptr :pointer) inner-ptr)
+        (c-ffmpeg-avdevice_list_devices context-ptr outer-ptr)
+        (make-ffmpeg-avdevice-info-list :ptr inner-ptr)))))
+
+;; (ffmpeg-avdevice-list-devices (ffmpeg-avformat-alloc-context))
